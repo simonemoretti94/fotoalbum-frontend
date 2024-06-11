56a33080a08ceecd9e81cf1d89dd56344edefb57
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-11 21:45:30 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
+<template>
+  <h1>hello fotoalbum-frontend!</h1>
+</template>
+
+<style scoped>
+h1 {
+  font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+}
+</style>
