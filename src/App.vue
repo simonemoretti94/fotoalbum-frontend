745707389ac548ef04e53c226707cfffd3ea92e5
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-16 19:20:29 +0200

    4th tested outside, now it works, next it will be binded with email system

--- src/App.vue
+++ src/App.vue
@@ -91,7 +91,71 @@ export default {
 
 <template>
 
+  <button class="btn btn-link position-fixed end-0" type="button" data-bs-toggle="offcanvas"
+    data-bs-target="#contactsForm" aria-controls="contactsForm">
+    Contacts
+  </button>
 
+  <div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="contactsForm"
+    aria-labelledby="staticBackdropLabel">
+    <div class="offcanvas-header">
+      <h5 class="offcanvas-title" id="staticBackdropLabel">
+        Get in touch
+      </h5>
+      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+      <div>
+        <div class="alert alert-success" role="alert" v-if="success">
+          <strong>Success</strong> {{ success }}
+        </div>
+
+        <div class="alert alert-danger" role="alert" v-if="errors">
+          <strong>There are some errors:</strong>
+          <ul>
+            <li v-for="error in errors">{{ error[0] }}</li>
+          </ul>
+        </div>
+
+
+        <p>Contact me, I'll get back as soon as possible</p>
+
+        <form @submit.prevent="submitMessage()">
+
+          <div class="mb-3">
+            <label for="email" class="form-label">E-mail</label>
+            <div class="mb-3">
+              <input type="text" class="form-control" name="email" id="email" aria-describedby="emailHelper"
+                placeholder="abc@mail.com" v-model="email" />
+              <small id="helpId" class="form-text text-muted">Type a valid e-mail</small>
+            </div>
+
+          </div>
+
+          <div class="mb-3">
+            <label for="name" class="form-label">Name</label>
+            <div class="mb-3">
+              <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelper"
+                placeholder="John Doe" v-model="name" />
+              <small id="helpId" class="form-text text-muted">Type your name and surname</small>
+            </div>
+
+          </div>
+
+          <div class="mb-3">
+            <label for="" class="form-label">Message</label>
+            <textarea class="form-control" name="message" id="message" rows="6" v-model="message"></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary" :class="{ 'disabled': this.loading }">
+            {{ loading ? 'Sending...📧' : 'Submit' }}
+          </button>
+
+        </form>
+
+      </div>
+
+    </div>
+  </div>
 
 
   <section class="container">
