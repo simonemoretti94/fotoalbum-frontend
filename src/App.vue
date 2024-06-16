2630de958235e9919b7b9973120fba57874ac311
simonemoretti94 <simone.moretti.944@gmail.com>
2024-06-16 19:19:52 +0200

    3rd fixing offcanvas

--- src/App.vue
+++ src/App.vue
@@ -91,71 +91,7 @@ export default {
 
 <template>
 
-  <button class="btn btn-link position-fixed end-0" type="button" data-bs-toggle="offcanvas"
-    data-bs-target="#contactsForm" aria-controls="contactsForm">
-    Contacts
-  </button>
 
-  <div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="contactsForm"
-    aria-labelledby="staticBackdropLabel">
-    <div class="offcanvas-header">
-      <h5 class="offcanvas-title" id="staticBackdropLabel">
-        Get in touch
-      </h5>
-      <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-    </div>
-    <div class="offcanvas-body">
-      <div>
-        <div class="alert alert-success" role="alert" v-if="success">
-          <strong>Success</strong> {{ success }}
-        </div>
-
-        <div class="alert alert-danger" role="alert" v-if="errors">
-          <strong>There are some errors:</strong>
-          <ul>
-            <li v-for="error in errors">{{ error[0] }}</li>
-          </ul>
-        </div>
-
-
-        <p>Contact me, I'll get back as soon as possible</p>
-
-        <form @submit.prevent="submitMessage()">
-
-          <div class="mb-3">
-            <label for="email" class="form-label">E-mail</label>
-            <div class="mb-3">
-              <input type="text" class="form-control" name="email" id="email" aria-describedby="emailHelper"
-                placeholder="abc@mail.com" v-model="email" />
-              <small id="helpId" class="form-text text-muted">Type a valid e-mail</small>
-            </div>
-
-          </div>
-
-          <div class="mb-3">
-            <label for="name" class="form-label">Name</label>
-            <div class="mb-3">
-              <input type="text" class="form-control" name="name" id="name" aria-describedby="nameHelper"
-                placeholder="John Doe" v-model="name" />
-              <small id="helpId" class="form-text text-muted">Type your name and surname</small>
-            </div>
-
-          </div>
-
-          <div class="mb-3">
-            <label for="" class="form-label">Message</label>
-            <textarea class="form-control" name="message" id="message" rows="6" v-model="message"></textarea>
-          </div>
-          <button type="submit" class="btn btn-primary" :class="{ 'disabled': this.loading }">
-            {{ loading ? 'Sending...📧' : 'Submit' }}
-          </button>
-
-        </form>
-
-      </div>
-
-    </div>
-  </div>
 
 
   <section class="container">
@@ -183,7 +119,78 @@ export default {
   </section>
 
 
+  <section v-if="photos">
+    <div class="container">
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-3 g-5">
+        <div class="col" v-for="photo in photos.data">
+          <div class="card" style="height: 450px;">
 
+            <template v-if="!photo.cover_image.startsWith('https://')">
+              <img :src="base_api_url + '/storage/' + photo.cover_image" :alt="photo.title" style="height: 80%;">
+            </template>
+            <template v-else>
+              <img :src="photo.cover_image" :alt="photo.title" style="height: 65%;">
+            </template>
+
+            <div class="card-body" style="height: 25%;">
+              <p><b>{{ photo.title }}</b><br>
+                {{ photo.description }}
+              </p>
+            </div>
+
+            <div class="card-footer" style="height: 10%;">
+
+              <!-- Modal trigger button -->
+              <button type="button" class="btn btn-primary btn-sm" data-bs-toggle="modal"
+                :data-bs-target="`#photo-${photo.id}`">
+                View
+              </button>
+
+              <div class="modal fade" :id="`photo-${photo.id}`" tabindex="-1" data-bs-backdrop="static"
+                data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+                <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-md" role="document">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" :id="`modal-title-${photo.id}`">
+                        {{ photo.title }}
+                      </h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="overflow-y: scroll;">
+
+                      <img v-if="!photo.cover_image.startsWith('https://')" class="img-fluid"
+                        :src="base_api_url + '/storage/' + photo.cover_image" :alt="photo.title" style="height: 80%;">
+                      <img v-else :src="photo.cover_image" class="img-fluid" :alt="photo.title" style="height: 80%;">
+                      <p>
+                        {{ photo.description }}
+                      </p>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
+                        Close
+                      </button>
+                      <!-- <button type="button" class="btn btn-primary">Save</button> -->
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <nav class="my-3" aria-label="Page navigation">
+        <ul class="pagination    ">
+          <li class="page-item" :class="{ 'd-none': !link.url, 'active': link.active }" v-for="link in photos.links">
+            <button class="page-link" :href="link.url" type="button" @click="goTo(link.url)"><span
+                v-html="link.label"></span></button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+
+
+
+  </section>
 
 </template>
 
